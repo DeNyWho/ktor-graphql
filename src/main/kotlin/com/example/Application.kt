d6bfd6bc6dc7a0741835769356f11f9aba30910c
DeNyWho <denis.akhunov00@mail.ru>
2022-03-02 16:21:27 +0500

    Update DessertSchema ( add update, delete, create methods )

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,13 +1,19 @@
 package com.example
 
+import com.example.di.mainModule
 import com.example.plugins.configureGraphQL
 import com.example.plugins.configureRouting
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.koin.core.context.startKoin
+import org.koin.ktor.ext.modules
 
 fun main() {
     embeddedServer(Netty, port = 8050, host = "0.0.0.0") {
         configureRouting()
+        startKoin {
+            modules(mainModule)
+        }
         configureGraphQL()
     }.start(wait = true)
 }
