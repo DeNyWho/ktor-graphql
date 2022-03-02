22dfbb77f969f51ce90785e9f8bae84f3e0e6666
Ахунов Денис <60143913+denywho@users.noreply.github.com>
2022-03-03 00:52:33 +0500

    Some changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val logbackVersion: String by project
 plugins {
     application
     kotlin("jvm") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 tasks {
