4c8a14e83a04e7c3788e9b92516a2997df22b98c
Ахунов Денис <60143913+denywho@users.noreply.github.com>
2022-03-03 00:52:33 +0500

    change maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,7 @@ repositories {
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
     maven { url = uri("https://repo1.maven.org/maven2/") }
     maven { url = uri("https://mvnrepository.com/artifact/io.insert-koin")}
+    maven { url = uri("https://mvnrepository.com/artifact/io.insert-koin/koin-android")}
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src/main")
